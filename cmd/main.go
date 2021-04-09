40ed5c4cf9469818db79d9400a1d270774cedc23
Ерузель Илья Константинович <4283-eik@AOI-TUSUR.LOCAL>
2021-04-09 09:27:04 +0700

    start

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
-	port:= ":8000"
+	os.Setenv("PORT", "4444")
+	port:= os.Getenv("PORT")
 
 	http.HandleFunc("/start", func(w http.ResponseWriter, _ *http.Request) {
 		fmt.Fprint(w, "Start")
 	} )
 
-	http.ListenAndServe(port, nil)
+	http.ListenAndServe(":"+port, nil)
 }
 

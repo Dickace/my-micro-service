76e4c5bbefb0612b55a6746ed5faa719de2a4394
Ерузель Илья Константинович <4283-eik@AOI-TUSUR.LOCAL>
2021-04-09 10:26:32 +0700

    some4

--- pkg/transport/handler.go
+++ pkg/transport/handler.go
@@ -2,14 +2,18 @@ package transport
 
 import (
 	"github.com/gorilla/mux"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 func Router() *mux.Router {
 	r:= mux.NewRouter()
 	s:= r.PathPrefix("/api/v1/").Subrouter()
 	s.HandleFunc("/", ServerOK).Methods(http.MethodHead)
-	s.HandleFunc("/api/v1", Arithmetic)
+	s.HandleFunc("/api/v1/arithmetic", Arithmetic).Methods(http.MethodPost)
 	return r
 }
 
@@ -18,5 +22,27 @@ func ServerOK(w http.ResponseWriter, _ *http.Request){
 }
 
 func Arithmetic(w http.ResponseWriter, r *http.Request)  {
-
+	requS, err:= ioutil.ReadAll(r.Body)
+	if err != nil{
+		log.Fatal(err)
+	}
+	str:= string(requS)
+	s:= strings.Fields(str)
+	sum, err:= strconv.ParseInt(s[0],10,64)
+	for i:= 1; i < len(s)+1;i+=2{
+		switch s[i] {
+		case "+":
+			v, err:= strconv.ParseInt(s[i+1],10,64)
+			if err!=nil{
+				log.Fatal(err)
+			}
+			sum+= v
+		case "-":
+			v, err:= strconv.ParseInt(s[i+1],10,64)
+			if err!=nil{
+				log.Fatal(err)
+			}
+			sum-= v
+		}
+	}
 }

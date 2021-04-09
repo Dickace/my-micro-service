b9eccfd72c71913438f7c076db0d330f1f746f06
Ерузель Илья Константинович <4283-eik@AOI-TUSUR.LOCAL>
2021-04-09 12:04:34 +0700

    some

--- pkg/transport/handler.go
+++ pkg/transport/handler.go
@@ -14,13 +14,7 @@ func Router() *mux.Router {
 	r:= mux.NewRouter()
 	s:= r.PathPrefix("/api/v1").Subrouter()
 	s.HandleFunc("/health", ServerOK).Methods(http.MethodHead)
-	s.HandleFunc("/arithmetic", func(writer http.ResponseWriter, request *http.Request) {
-		requestS, _:= ioutil.ReadAll(request.Body)
-		log.Print(requestS)
-		str:= string(requestS)
-		log.Print(str)
-		fmt.Fprintf(writer,"4")
-	}).Methods(http.MethodPost)
+	s.HandleFunc("/arithmetic", Arithmetic).Methods(http.MethodPost)
 	return r
 }
 
@@ -35,23 +29,18 @@ func Arithmetic(w http.ResponseWriter, r *http.Request)  {
 		log.Fatal(err)
 	}
 	str:= string(requS)
-	s:= strings.Fields(str)
-	sum, err:= strconv.ParseInt(s[0],10,64)
-	for i:= 1; i < len(s)+1;i+=2{
-		switch s[i] {
-		case "+":
-			v, err:= strconv.ParseInt(s[i+1],10,64)
-			if err!=nil{
-				log.Fatal(err)
-			}
-			sum+= v
-		case "-":
-			v, err:= strconv.ParseInt(s[i+1],10,64)
-			if err!=nil{
-				log.Fatal(err)
-			}
-			sum-= v
-		}
+	s:= strings.Split(str, "+")
+	sum:= int64(0)
+	for i:=0; i< len(s);i++{
+		t, _ := strconv.ParseInt(s[i], 10,64)
+		sum += t
 	}
-	fmt.Fprintf(w, strconv.FormatInt(sum, 10))
+
+
+	fmt.Fprintf(w, strconv.Itoa(int(sum)))
+}
+
+func SplitMultyply(s string, sum int) int {
+
+	return sum
 }
